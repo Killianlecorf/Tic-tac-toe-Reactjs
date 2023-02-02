a7e70694302b07dc306747867143469f6bdcbd41
killian-lecorf <killianlecorf@outlook.fr>
2023-02-02 19:41:36 +0100

    board size

--- src/classes/Board.js
+++ src/classes/Board.js
@@ -1,14 +1,31 @@
 class Board{
 
-    constructor() {
-        this.grid = [["" , "" , ""] , ["" , "" , ""] , ["" , "" , ""]]
+    constructor(size = 3) {
+        this.grid = this.creationGrid(size)
     }
 
     add(playerMove , y , x){
         this.grid[y][x] = playerMove
     }
 
-   
+    
+    creationGrid(size){
+
+        let grid = []
+        let line = []
+
+        for (let x = 0; x < size; x++) {
+            line.push("") 
+        }
+
+        for (let y = 0; y < size; y++) {
+            grid.push(line)
+        }
+        return grid
+    }
+
+ 
 }
+
 
 export default Board;
--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -6,6 +6,12 @@ const PlayGame = () => {
     const [player , setPlayer] = useState(1)
     const [board , setBoard] = useState(new Board())
 
+    const handleChangeCase = (event) => {
+        // const DisplayNumberCase = parseInt(event.target.value)
+        setBoard(new Board())
+        
+    }
+
 
     const handleRestartGame = () => {
         setBoard(new Board())
@@ -50,6 +56,52 @@ const PlayGame = () => {
         switchPlayer()
     }
 
+    const winVerification = (x ,y) => {
+        // const size = 3
+        // let d = 0
+        // for (let y = 0; x < size; y++) {
+        //     for (let x = 0; x < size; x++) {
+        //         if (board.grid[y][x] === 3) {
+
+        //             d++
+        //         }else{
+        //             d=0
+        //         }
+        //     }
+        // }
+            for (let y = 0; y < 3; y++) {
+                if (!board.grid[y][0]) {
+                    continue
+                }
+                if (board.grid[y][0] === board.grid[y][1]  && board.grid[y][1] === board.grid[y][2]) {
+                    return true
+                }
+            }
+    
+            for (let x = 0; x < 3; x++) {
+                if (!board.grid[0][x]) {
+                    continue
+                }
+                if (board.grid[0][x] === board.grid[1][x] && board.grid[1][x] === board.grid[2][x]) {
+                    return true
+                }
+            }
+    
+            if (board.grid[0][0] === board.grid[1][1] && board.grid[1][1] === board.grid[2][2] && board.grid[2][2]) {
+                return true
+            }  
+            
+            if (board.grid[0][2]) {
+                if (board.grid[2][0] === board.grid[1][1] && board.grid[1][1] === board.grid[0][2]) {
+                    return true
+                }
+            }
+    
+            return false
+        
+    }
+
+
     const displayCase = () => {
         
         return board.grid.map((ligne, y) => ligne.map(
@@ -65,38 +117,7 @@ const PlayGame = () => {
     }
     
 
-    const winVerification = () => {
-        
-        for (let y = 0; y < 3; y++) {
-            if (!board.grid[y][0]) {
-                continue
-            }
-            if (board.grid[y][0] === board.grid[y][1]  && board.grid[y][1] === board.grid[y][2]) {
-                return true
-            }
-        }
-
-        for (let x = 0; x < 3; x++) {
-            if (!board.grid[0][x]) {
-                continue
-            }
-            if (board.grid[0][x] === board.grid[1][x] && board.grid[1][x] === board.grid[2][x]) {
-                return true
-            }
-        }
-
-        if (board.grid[0][0] === board.grid[1][1] && board.grid[1][1] === board.grid[2][2] && board.grid[2][2]) {
-            return true
-        }  
-        
-        if (board.grid[0][2]) {
-            if (board.grid[2][0] === board.grid[1][1] && board.grid[1][1] === board.grid[0][2]) {
-                return true
-            }
-        }
-
-        return false
-    }
+    
 
 
     const createViewVerification = (player ,winnerPlayer) => {
@@ -133,6 +154,12 @@ const PlayGame = () => {
                     </div>
                     <div className="win-para">
                         {createViewVerification(player)}
+                    </div>
+                    <div className="select-case">
+                        <select name="" onChange={ handleChangeCase }>
+                            <option value="3">par 3</option>
+                            <option value="4">par 4</option>
+                        </select>
                     </div>
                     <div className="select-content">
                         <select onChange={ handleChangeInput } >
