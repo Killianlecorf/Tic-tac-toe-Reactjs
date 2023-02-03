838d04341fff7b6b26f71cb45aa6a169c591a415
killian-lecorf <killianlecorf@outlook.fr>
2023-02-03 18:43:00 +0100

    add change board size

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -3,28 +3,42 @@ import Board from '../classes/Board';
 
 const PlayGame = () => {
 
+    const [winSize , setwinSize] = useState(3)
+    const [boardSize , setBoardSize] = useState(3)
+    const [sizePourcent , setSizePourcent] = useState('30%')
     const [player , setPlayer] = useState(1)
-    const [board , setBoard] = useState(new Board())
+    const [board , setBoard] = useState(new Board(3))
+    const [gameState , setGameState] = useState({
+        isWin: false,
+        isNull: false
+    })
+
 
     const handleChangeCase = (event) => {
-        // const DisplayNumberCase = parseInt(event.target.value)
-        setBoard(new Board())
-        
+        const size = parseInt(event.target.value)
+        setBoard(new Board(size))
+        if (sizePourcent === '30%') {
+            setSizePourcent('20%')
+            setBoardSize(4)
+        }
+        else{
+            setSizePourcent('30%')
+            setBoardSize(3)
+        }
     }
 
 
     const handleRestartGame = () => {
-        setBoard(new Board())
+        setBoard(new Board(boardSize))
         setPlayer(1) 
     }
 
     const handleChangeInput = (event) => {
-        let test = event.target.value
-        let ParseEvent = parseInt(test);
+        const findPlayer = event.target.value
+        let ParseEvent = parseInt(findPlayer);
         setPlayer(ParseEvent);
-        handleRestartGame()
+        
         }
-        console.log(player);
 
     const switchPlayer = () => {
         if (player === 1 ) {
@@ -34,112 +48,137 @@ const PlayGame = () => {
         if (player === 2 ) {
             setPlayer(1)
         }
+        
     }
  
-    const addPlayer = (x, y , winnerPlayer) => {
+
+
+    const addPlayer = (x, y) => {
+
+        let isWin = winVerification()
+
+        if (isWin === false) {
+            if (player === 1) {
+                board.add('X' , y, x)
+            }
+            if (player === 2) {
+                board.add('O' , y, x)
+            }
+            switchPlayer()
+        }
+
+        if (isWin === true) {
+            setGameState({...gameState, isWin } )
+            return
+        }
         
-        winnerPlayer = winVerification()
 
-        if (winnerPlayer === true) {
-            return
-        }
-
-        if (board.grid[y][x]) {
-            return
-        }
-        if (player === 1) {
-            board.add('X' , y, x)
-        }
-        if (player === 2) {
-            board.add('O' , y, x)
-        }
-        switchPlayer()
-    }
-
-    const winVerification = (x ,y) => {
-        // const size = 3
-        // let d = 0
-        // for (let y = 0; x < size; y++) {
-        //     for (let x = 0; x < size; x++) {
-        //         if (board.grid[y][x] === 3) {
-
-        //             d++
-        //         }else{
-        //             d=0
-        //         }
-        //     }
+        // if (player === 1) {
+        //     board.add('X' , y, x)
         // }
-            for (let y = 0; y < 3; y++) {
-                if (!board.grid[y][0]) {
-                    continue
-                }
-                if (board.grid[y][0] === board.grid[y][1]  && board.grid[y][1] === board.grid[y][2]) {
-                    return true
-                }
-            }
+        // if (player === 2) {
+        //     board.add('O' , y, x)
+        // }
+
+        // let isWin = winVerification()
+
+        // if (isWin === true) {
+        //     setGameState({...gameState, isWin } )
+        //     return
+        // }
     
-            for (let x = 0; x < 3; x++) {
-                if (!board.grid[0][x]) {
-                    continue
-                }
-                if (board.grid[0][x] === board.grid[1][x] && board.grid[1][x] === board.grid[2][x]) {
-                    return true
-                }
-            }
-    
-            if (board.grid[0][0] === board.grid[1][1] && board.grid[1][1] === board.grid[2][2] && board.grid[2][2]) {
-                return true
-            }  
-            
-            if (board.grid[0][2]) {
-                if (board.grid[2][0] === board.grid[1][1] && board.grid[1][1] === board.grid[0][2]) {
-                    return true
-                }
-            }
-    
-            return false
-        
+        // switchPlayer()
+
     }
+
+    // const winVerification = () => {
+    //     for (let y = 0; y < 3; y++) {
+    //         if (!board.grid[y][0]) {
+    //             continue
+    //         }
+    //         if (board.grid[y][0] === board.grid[y][1]  && board.grid[y][1] === board.grid[y][2]) {
+    //             return true
+    //         }
+    //     }
+
+    //     for (let x = 0; x < 3; x++) {
+    //         if (!board.grid[0][x]) {
+    //             continue
+    //         }
+    //         if (board.grid[0][x] === board.grid[1][x] && board.grid[1][x] === board.grid[2][x]) {
+    //             return true
+    //         }
+    //     }
+
+    //     if (board.grid[0][0] === board.grid[1][1] && board.grid[1][1] === board.grid[2][2] && board.grid[2][2]) {
+    //         return true
+    //     }  
+        
+    //     if (board.grid[0][2]) {
+    //         if (board.grid[2][0] === board.grid[1][1] && board.grid[1][1] === board.grid[0][2]) {
+    //             return true
+    //         }
+    //     }
+
+    //     return false
+
+    // }
 
 
     const displayCase = () => {
-        
+
         return board.grid.map((ligne, y) => ligne.map(
             (square, x) => 
                 <div 
                     key={x + '-' + y}
                     className="case"
                     onClick={() => addPlayer(x, y)}
+                    style = {{ width : sizePourcent }}
                 >
                     <p className='square'>{square}</p>
                 </div>
         ));
     }
-    
-
-    
 
 
-    const createViewVerification = (player ,winnerPlayer) => {
+    const createViewVerification = (player) => {
 
-        winnerPlayer = winVerification()
-        
         if (player === 1 ) {
-            player = 2
-            if (winnerPlayer === true) {
+            if ( gameState.isWin === true) {
             
                 return <p>joueur { player } à gagné</p>
             }
             return ''
         }
         if (player === 2 ) {
-            player = 1
-            if (winnerPlayer === true) {
+            if (gameState.isWin === true) {
             
                 return <p>joueur { player } à gagné</p>
             }
             return ''
         }
+    }
+
+    const handleChangeCasesize = () => {
+
+        if (winSize === 3) {
+            setwinSize(4)
+        }
+        if (winSize === 3) {
+            setwinSize(4)
+        }
+
+    }
+
+    const winVerification = () => {
+        
+        // d=0
+
+        // for (let y = 0; y < sizeWin; y++) {
+            
+            
+        // }
+
     }
 
     return (
@@ -156,12 +195,21 @@ const PlayGame = () => {
                         {createViewVerification(player)}
                     </div>
                     <div className="select-case">
+                        <label className='label-grid-size'>Taille de la grille</label>
                         <select name="" onChange={ handleChangeCase }>
                             <option value="3">par 3</option>
                             <option value="4">par 4</option>
                         </select>
                     </div>
+                    <div className="select-case">
+                        <label className='label-grid-size'>Taille pour gagner</label>
+                        <select name="" onChange={ handleChangeCasesize }>
+                            <option value="3">par 3</option>
+                            <option value="4">par 4</option>
+                        </select>
+                    </div>
                     <div className="select-content">
+                    <label className='label-grid-size'>Choix du joueur</label>
                         <select onChange={ handleChangeInput } >
                             <option value="1">Joueur 1</option>
                             <option value="2">Joueur 2</option>
